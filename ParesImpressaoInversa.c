151a5fadade2332a44578e6430325b5e14758673
Danielprogrammer01 <dannielsantoscorreia@gmail.com>
2023-11-14 16:39:20 -0300

    conclusao da atividade

--- ParesImpressaoInversa.c
+++ ParesImpressaoInversa.c
@@ -9,7 +9,35 @@ int main()
 {
     setlocale(LC_ALL, "portuguese");
 
-    
+    int i;
+    int quantidade = 6;
+    int numeros[quantidade];
+
+    for (i = 0; i < quantidade; i++)
+    {
+
+        do
+        {
+
+            printf("\nDigite o %dº número: ", i + 1);
+            scanf("%d", &numeros[i]);
+
+            if (numeros[i] % 2 != 0)
+            {
+                printf("\nNÚMERO INVÁLIDO! DIGITE UM NÚMERO PAR.\n");
+            }
+
+        } while (numeros[i] % 2 != 0);
+    }
+
+    printf("\nNúmeros obtidos: \n");
+
+    for (i = (quantidade - 1); i >= 0; i--)
+    {
+        printf("\n%d", numeros[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
